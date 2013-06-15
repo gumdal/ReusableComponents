309b6190e20980df746f6a7ae5263a855cb5f6df
Raj Pawan Gumdal <rajpawangumdal@Rajs-MacBook-Pro.local>
2013-06-15 19:00:55 +0530

    Update js by removing the alerts

--- Java Script/ImageAspectRatio.js
+++ Java Script/ImageAspectRatio.js
@@ -2,13 +2,10 @@
 function resizeToMax(id){
     myImage = new Image()
     var img = document.getElementById(id);
-    alert ("Hello world");
     myImage.src = img.src;
     if(myImage.width / document.body.clientWidth > myImage.height / document.body.clientHeight){
         img.style.width = "100%";
-        alert ("Width is more");
     } else {
         img.style.height = "100%";
-        alert ("Height is more");
     }
 }
